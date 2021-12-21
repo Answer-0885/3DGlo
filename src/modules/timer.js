76230e97fca60536e3755d6b4e41587b4a4d3dfb
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-21 21:33:44 +0700

    обязательная домашка по 19 уроку 3DGlo

---
+++ src/modules/timer.js
@@ -0,0 +1,47 @@
+const timer = (deadLine) => {
+   const timerHours = document.getElementById('timer-hours');
+   const timerMinutes = document.getElementById('timer-minutes');
+   const timerSeconds = document.getElementById('timer-seconds');
+
+   const getTimeRemaining = () => {
+      let dateStop = new Date(deadLine).getTime();
+      let dateNow = new Date().getTime();
+      let timeRemaining = (dateStop - dateNow) / 1000;
+      let days = Math.floor(timeRemaining / 60 / 60 / 24);
+      let hours = Math.floor((timeRemaining / 60 / 60) % 24);
+      let minutes = Math.floor((timeRemaining / 60) % 60);
+      let seconds = Math.floor(timeRemaining % 60)
+
+      return {
+         timeRemaining,
+         hours,
+         minutes,
+         seconds
+      }
+   };
+   const updateClock = () => {
+      let getTime = getTimeRemaining();
+      // Добавляем нули перед цифрами
+      function addZero(num) {
+         if (num <= 9) {
+            return '0' + num;
+         } else return num;
+      };
+
+      timerHours.textContent = addZero(getTime.hours);
+      timerMinutes.textContent = addZero(getTime.minutes);
+      timerSeconds.textContent = addZero(getTime.seconds);
+
+      if (getTime.timeRemaining <= 0) {
+         clearInterval(updateClock);
+         timerHours.textContent = '00';
+         timerMinutes.textContent = '00';
+         timerSeconds.textContent = '00';
+      }
+      console.log(getTime.seconds);
+   }
+   setInterval(updateClock, 1000);
+}
+
+
+export default timer
