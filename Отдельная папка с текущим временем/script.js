3af8c4d2f0e568ebec5a284b38ab9ae58c4f5b15
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-22 00:47:09 +0700

    Дополнительная Домашка по 19 уроку в отдельной папке

--- Отдельная папка с текущим временем/script.js
+++ Отдельная папка с текущим временем/script.js
@@ -11,7 +11,6 @@ function clock1() {
    mins = d.getMinutes();
    sec = d.getSeconds();
 
-
    // приветствие в зависимости от времени суток
    if (hours >= 5 && hours < 12) greeting = "Доброе утро!";
    else {
@@ -47,75 +46,3 @@ function clock1() {
    console.log(hours);
 }
 setInterval(clock1, 1000);
-
-
-
-//  б)'04.02.2020 - 21:05:33'
-// Для вывода в формате(б) напишите функцию, которая будет добавлять 0 перед значениями которые состоят из одной цифры(из 9: 5: 3 1.6 .2019 сделает 09: 05: 03 01.06 .2019)
-
-
-
-function clock2() {
-   d = new Date();
-   year = d.getFullYear();
-   month = d.getMonth();
-   day = d.getDate();
-   hour = d.getHours();
-   minutes = d.getMinutes();
-   seconds = d.getSeconds();
-
-   // проверка если цифра до 9 включительно, то прибавляем 0 спереди
-   function addZero(num) {
-      if (num >= 0 && num <= 9) {
-         return '0' + num;
-      } else {
-         return num;
-      }
-   }
-
-   switch (month) {
-      case 0:
-         fMonth = "1";
-         break;
-      case 1:
-         fMonth = "2";
-         break;
-      case 2:
-         fMonth = "3";
-         break;
-      case 3:
-         fMonth = "4";
-         break;
-      case 4:
-         fMonth = "5";
-         break;
-      case 5:
-         fMonth = "6";
-         break;
-      case 6:
-         fMonth = "7";
-         break;
-      case 7:
-         fMonth = "8";
-         break;
-      case 8:
-         fMonth = "9";
-         break;
-      case 9:
-         fMonth = "10";
-         break;
-      case 10:
-         fMonth = "11";
-         break;
-      case 11:
-         fMonth = "12";
-         break;
-   }
-
-
-   currentTime = "Текущее время: " + addZero(day) + "." + addZero(fMonth) + "." + addZero(year) + " - " + addZero(hour) + ":" + addZero(minutes) + ":" + addZero(seconds)
-
-   forDays.textContent = currentTime;
-
-}
-setInterval(clock2, 1000);
