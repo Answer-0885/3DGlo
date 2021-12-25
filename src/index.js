81bf3c634c74c9407f6c5861ea0262e0d7631b79
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-25 21:38:03 +0700

    обязательная домашка по 21 уроку

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import check from './modules/check'
 
 
 timer('31 december 2021');
 menu();
 modal();
+check();
