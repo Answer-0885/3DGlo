e1f7d64261a92dfcca82207b7596715cae09a068
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-29 19:46:57 +0700

    23 урок практика

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import menu from './modules/menu'
 import modal from './modules/modal'
 import check from './modules/check'
 import tabs from './modules/tabs'
+import slider from './modules/slider'
 
 
 timer('31 december 2021');
@@ -10,3 +11,4 @@ menu();
 modal();
 check();
 tabs();
+slider();
