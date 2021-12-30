0939095469ff4511e35a36965facb33b0d0b72fe
Vitaly Zakatov <answer-0885@mail.ru>
2021-12-30 23:10:03 +0700

    Домашка по 24 уроку

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -8,7 +8,28 @@ const calc = (price = 100) => {
 
    const countCalc = () => {
       const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
-      console.log(calcTypeValue);
+      const calcSquareValue = calcSquare.value;
+
+      let totalValue = 0;
+      let calcCountValue = 1;
+      let calcDayValue = 1;
+
+      if (calcDay.value && calcDay.value < 5) {
+         calcDayValue = 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+         calcDayValue = 1.5;
+      }
+
+      if (calcCount.value > 1) {
+         calcCountValue += +calcCount.value / 10;
+      }
+
+      if (calcType.value && calcSquare.value) {
+         totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue
+      } else {
+         totalValue = 0;
+      }
+      total.textContent = totalValue;
    }
    calcBlock.addEventListener('input', (e) => {
       if (e.target === calcType || e.target === calcSquare ||
