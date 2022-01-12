a419d1b86a4a335aa881f748091aaadec0e43dd4
Vitaly Zakatov <answer-0885@mail.ru>
2022-01-12 15:35:29 +0700

    обязательная домашка по 25 уроку

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -4,7 +4,9 @@ const calc = (price = 100) => {
    const calcSquare = document.querySelector('.calc-square');
    const calcCount = document.querySelector('.calc-count');
    const calcDay = document.querySelector('.calc-day');
-   const total = document.getElementById('total')
+   const total = document.querySelector('#total');
+
+
 
    const countCalc = () => {
       const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
@@ -14,29 +16,47 @@ const calc = (price = 100) => {
       let calcCountValue = 1;
       let calcDayValue = 1;
 
+
+      if (calcCount.value > 1) {
+         calcCountValue += +calcCount.value / 10;
+      }
+
       if (calcDay.value && calcDay.value < 5) {
          calcDayValue = 2;
       } else if (calcDay.value && calcDay.value < 10) {
          calcDayValue = 1.5;
       }
 
-      if (calcCount.value > 1) {
-         calcCountValue += +calcCount.value / 10;
-      }
-
       if (calcType.value && calcSquare.value) {
-         totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue
+         totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
       } else {
          totalValue = 0;
       }
-      total.textContent = totalValue;
-   }
+
+      //переменные для анимации
+      let end = 0,
+         step = 50,
+         animInterval = 0;
+
+      function animate() {
+         end += step;
+         if (end >= totalValue) {
+            end = totalValue;
+            cancelAnimationFrame(animInterval);
+         }
+         total.textContent = end;
+         animInterval = requestAnimationFrame(animate);
+      }
+
+      animInterval = requestAnimationFrame(animate);
+   };
+
    calcBlock.addEventListener('input', (e) => {
       if (e.target === calcType || e.target === calcSquare ||
          e.target === calcCount || e.target === calcDay) {
          countCalc();
       }
-   })
-}
+   });
+};
 
-export default calc
+export default calc;
