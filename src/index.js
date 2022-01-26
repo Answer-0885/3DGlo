b52d6df3a9ad6ebf285bba27d1588e2cec711661
Vitaly Zakatov <answer-0885@mail.ru>
2022-01-26 20:16:32 +0700

    Проверка финальной версии

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import calc from './modules/calc'
 import sendForm from './modules/sendForm'
 
 
+
 timer('31 january 2022');
 menu();
 modal();
@@ -15,24 +16,4 @@ check();
 tabs();
 slider();
 calc(100);
-sendForm({
-   formId: 'form1',
-   someElem: [{
-      type: 'block',
-      id: 'total'
-   }]
-});
-sendForm({
-   formId: 'form2',
-   someElem: [{
-      type: 'block',
-      id: 'total'
-   }]
-});
-sendForm({
-   formId: 'form3',
-   someElem: [{
-      type: 'block',
-      id: 'total'
-   }]
-});
+sendForm();
