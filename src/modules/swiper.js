5a61bc0fd5cfa17514d7b3a460cfa658e00a4635
Vitaly Zakatov <answer-0885@mail.ru>
2022-01-27 11:28:58 +0700

    Форма не работает, но сделал свайпер

---
+++ src/modules/swiper.js
@@ -0,0 +1,23 @@
+const swiper = () => {
+   const swiper = new Swiper('.swiper', {
+      // navigation: {
+      //    nextEl: '.benefits__arrow--right',
+      //    prevEl: '.benefits__arrow--left',
+      // },
+      grabCursor: true,
+      loop: true,
+      autoplay: {
+         delay: 2500,
+         disableOnInteraction: false,
+      },
+      breakpoints: {
+
+         // Если ширина окна больше или равна 576px
+         576: {
+            slidesPerView: 3,
+            spaceBetween: 40
+         }
+      }
+   });
+};
+export default swiper
